44464126c5d609dd6a390d91c998dc0f1a2faee3
Grzegorz Kruk <poczta@grzegorzkruk.pl>
2017-05-30 14:24:50 +0200

    Partly fixed slick image slider

--- app/scripts/directives/slick.directive.js
+++ app/scripts/directives/slick.directive.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('ekoWebSite')
-    .directive('slickSlider', function ($animate, $rootScope, $timeout, $location) {
+    .directive('slickSlider', function ($animate, $rootScope, $timeout) {
         var link = function(scope, element){
+            var oSliderFor = angular.element(element).find('.slider-for'),
+                oSliderNav = angular.element(element).find('.slider-nav');
             angular.element(element).find('.slider-for').slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
@@ -10,7 +12,7 @@ angular.module('ekoWebSite')
                 arrows: false,
                 asNavFor: '.slider-nav'
             });
-            angular.element(element).find('.slider-nav').slick({
+            $(oSliderNav).slick({
                 slidesToShow: 3,
                 slidesToScroll: 1,
                 asNavFor: '.slider-for',
@@ -23,7 +25,7 @@ angular.module('ekoWebSite')
             restrict: 'E',
             templateUrl: 'views/slick-slider.html',
             scope:{
-                images: '@'
+                images: '='
             },
             link: link
         };
